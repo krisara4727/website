bdbd9da9517e4a74bd210d5ea130373668d3aaea
krisara4727 <chivtekrisara.4727@gmail.com>
2020-03-16 13:08:32 +0530

    file upload database

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
-
+const bookRouter = require('./routes/books')
 
 
 app.set('view engine', 'ejs')
@@ -33,4 +34,6 @@ db.once('open', () => console.log('connected to database'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
+
 app.listen(process.env.PORT || 3000)
